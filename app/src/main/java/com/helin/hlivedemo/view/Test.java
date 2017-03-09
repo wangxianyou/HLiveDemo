bbaf1a135be20c56e43e0c33c4ca96b45161c44d
wangxianyou <303681285@qq.com>
2017-03-09 21:44:55 +0800

    eee

--- app/src/main/java/com/helin/hlivedemo/view/Test.java
+++ app/src/main/java/com/helin/hlivedemo/view/Test.java
@@ -3,6 +3,7 @@ package com.helin.hlivedemo.view;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 /**
  * Created by wangxianyou on 2017/3/9.
@@ -12,5 +13,6 @@ public class Test extends Activity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.e("mmm", "onCreate: ");
     }
 }
